908e18bcb03d1bedd17b7b5b289eb0c5d13242a0
Nick Long <nick.long@rtslabs.com>
2018-05-29 14:17:57 -0400

    solved capstone 5!

---
+++ Core/Api.fs
@@ -0,0 +1,50 @@
+﻿/// Provides access to the banking API.
+module Capstone5.Api
+
+open Capstone5.Domain
+open Capstone5.Operations
+open Capstone5.FileRepository
+open System
+
+let withdrawWithAudit amount (CreditAccount account as creditAccount) =
+    auditAs "withdraw" Auditing.composedLogger withdraw amount creditAccount account.AccountId account.Owner
+
+let depositWithAudit amount (ratedAccount:RatedAccount) =
+    let accountId = ratedAccount.GetField(fun a -> a.AccountId)
+    let owner = ratedAccount.GetField(fun a -> a.Owner)
+    auditAs "deposit" Auditing.composedLogger deposit amount ratedAccount accountId owner
+
+let internal getAccountFromName name =
+    let transactions = tryFindTransactionsOnDisk name
+    match transactions with
+    | Some accountInfo -> loadAccount accountInfo
+    | None -> loadAccount (name, Guid.NewGuid(), [])
+
+/// Loads an account from disk. If no account exists, an empty one is automatically created.
+let LoadAccount (customer:Customer) : RatedAccount =
+    getAccountFromName customer.Name
+
+/// Deposits funds into an account.
+let Deposit (amount:decimal) (customer:Customer) : RatedAccount =
+    getAccountFromName customer.Name
+    |> depositWithAudit amount
+
+/// Gets the balance from an account
+let GetBalance account =
+    match account with
+    | Overdrawn account -> account.Balance
+    | InCredit (CreditAccount account) -> account.Balance
+
+/// Withdraws funds from an account that is in credit.
+let Withdraw (amount:decimal) (customer:Customer) : RatedAccount =
+    let account = getAccountFromName customer.Name
+    match account with
+    | InCredit (CreditAccount account as creditAccount) -> withdrawWithAudit amount creditAccount
+    | Overdrawn _ -> account
+                                 
+/// Loads the transaction history for an owner. If no transactions exist, returns an empty sequence.
+let LoadTransactionHistory(customer:Customer) : Transaction seq =
+    match tryFindTransactionsOnDisk customer.Name with
+    | None -> Seq.empty
+    | Some (_,_,transactions) -> transactions
+
---
+++ Core/Domain.fs
@@ -0,0 +1,29 @@
+namespace Capstone5.Domain
+
+open System
+open Newtonsoft.Json
+
+type BankOperation = Deposit | Withdraw
+type Customer = { Name : string }
+type Account = { AccountId : Guid; Owner : Customer; Balance : decimal }
+type Transaction = { Timestamp : DateTime; Operation : string; Amount : decimal }
+
+/// Represents a bank account that is known to be in credit.
+type CreditAccount = CreditAccount of Account
+/// A bank account which can either be in credit or overdrawn.
+type RatedAccount =
+    | InCredit of CreditAccount
+    | Overdrawn of Account
+    member this.GetField getter =
+        match this with
+        | InCredit (CreditAccount account) -> getter account
+        | Overdrawn account -> getter account
+
+module Transactions =
+    /// Serializes a transaction
+    let serialize transaction =
+        JsonConvert.SerializeObject transaction
+    
+    /// Deserializes a transaction
+    let deserialize fileContents =
+        JsonConvert.DeserializeObject<Transaction> fileContents
